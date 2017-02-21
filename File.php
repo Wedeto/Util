cbbfcb964d829fea32f3e7f7d90130e74e908718
Egbert van der Wal <ewal@pointpro.nl>
2017-02-21 11:56:23 +0100

    Debugging changes

--- File.php
+++ File.php
@@ -25,8 +25,14 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 namespace WASP\Util;
 
+use WASP\PermissionError;
+use WASP\Debug\LoggerAwareStaticTrait;
+use Throwable;
+
 class File
 {
+    use LoggerAwareStaticTrait;
+
     private $dir;
     private $path;
     private $filename;
@@ -36,6 +42,18 @@ class File
 
     private static $file_group = null;
     private static $file_mode = null;
+
+    const OWNER_READ = 0400;
+    const OWNER_WRITE = 0200;
+    const OWNER_EXECUTE = 0100;
+
+    const GROUP_READ = 0040;
+    const GROUP_WRITE = 0020;
+    const GROUP_EXECUTE = 0010;
+
+    const WORLD_READ = 0040;
+    const WORLD_WRITE = 0020;
+    const WORLD_EXECUTE = 0010;
 
     public function __construct(string $filename, $mime = null)
     {
@@ -70,19 +88,124 @@ class File
     {
         self::$file_mode = $mode;
     }
+    
+    public static function getPermissions($path)
+    {
+        try
+        {
+            $file_stat = @fileperms($path);
+        }
+        catch (Throwable $e)
+        {
+            throw new IOException("Could not stat: " . $path);
+        }
+
+        $mode = $file_stat['mode'];
+        
+        $perms = array(
+            'owner' => array(
+                'read'    => $mode & 0x0100,
+                'write'   => $mode & 0x0080,
+                'execute' => $mode & 0x0040
+            ),
+            'group' => array(
+                'read'    => $mode & 0x0020,
+                'write'   => $mode & 0x0010,
+                'execute' => $mode & 0x0008
+            ),
+            'world' => array(
+                'read'    => $mode & 0x0004,
+                'write'   => $mode & 0x0002,
+                'execute' => $mode & 0x0001
+            )
+        );
+
+        $fmode = 0;
+        $fmode |= $perms['owner']['read']    ? self::OWNER_READ    : 0;
+        $fmode |= $perms['owner']['write']   ? self::OWNER_WRITE   : 0;
+        $fmode |= $perms['owner']['execute'] ? self::OWNER_EXECUTE : 0;
+
+        $fmode |= $perms['group']['read']    ? self::GROUP_READ    : 0;
+        $fmode |= $perms['group']['write']   ? self::GROUP_WRITE   : 0;
+        $fmode |= $perms['group']['execute'] ? self::GROUP_EXECUTE : 0;
+
+        $fmode |= $perms['world']['read']    ? self::WORLD_READ    : 0;
+        $fmode |= $perms['world']['write']   ? self::WORLD_WRITE   : 0;
+        $fmode |= $perms['world']['execute'] ? self::WORLD_EXECUTE : 0;
+
+        $perms['mode'] = $fmode;
+        return $perms;
+    }
+
+    public static function makeWritable($path)
+    {
+        $perms = self::getPermissions($path);
+        $current_user = posix_getpwnam($_SERVER['USER']);
+        $owner = posix_getpwuid(fileowner($path));
+        $group = posix_getgrgid(filegroup($path));
+
+        if ($current_user['uid'] !== $owner['uid'])
+            throw new PermissionError($this->path, "Cannot change permissions - not the owner");
+
+        // We own the file, so we should be able to fix it
+        $set_gid = false;
+        if (self::$file_group !== null)
+        {
+            if (self::$file_group !== $group['name'] && !chgrp($path, self::$file_group))
+                throw new PermissionError($this->path, "Cannot change group");
+            $set_gid = true;
+        }
+
+        // Owner and group are all right now, we should be able to modify the permissions
+        $new_mode = $perms['mode'] | self::OWNER_WRITE | ($set_gid ? self::GROUP_WRITE : 0);
+
+        if (is_dir($path))
+            $new_mode |= self::OWNER_EXECUTE | ($set_gid ? self::GROUP_EXECUTE : 0);
+
+        if ($new_mode === $perms['mode'])
+            return;
+
+        $what = is_dir($path) ? "directory" : "file";
+        self::$logger("Changing permissions of {0} {1} to {2} (was: {3})", $what, $path, $new_mode, $perms['mode']);
+
+        try
+        {
+            @chmod($path, $new_mode);
+        }
+        catch (Throwable $e)
+        {
+            throw new PermissionError($path, "Could not set permissions");
+        }
+    }
 
     public function touch()
     {
+        // Check permissions
+        if (file_exists($this->path))
+        {
+            if (!is_writable($this->path))
+                self::makeWritable($this->path);
+        }
+
         touch($this->path);
         $this->setPermissions();
     }
 
     public function setPermissions()
     {
-        if (isset(self::$file_group))
-            @chgrp($this->path, self::$file_group);
-        if (isset(self::$file_mode))
-            @chmod($this->path, self::$file_mode);
+        try
+        {
+            if (isset(self::$file_group))
+                @chgrp($this->path, self::$file_group);
+            if (isset(self::$file_mode))
+                @chmod($this->path, self::$file_mode);
+        }
+        catch (Throwable $e)
+        {
+            self::$logger->critical("Failed to set permissions on {0}", $this->path);
+            self::$logger->critical($e);
+            throw new PermissionError($this->path);
+        }
     }
 
     public function getExt()
@@ -142,3 +265,7 @@ class File
         return $this->basename;
     }
 }
+
+// @codeCoverageIgnoreStart
+File::setLogger();
+// @codeCoverageIgnoreEnd
